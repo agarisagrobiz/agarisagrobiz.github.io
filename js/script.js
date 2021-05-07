057401c55ad36f79b9bc9d18e0e63b0d25d08098
Agaris Agrobiz <83822026+agarisagrobiz@users.noreply.github.com>
2021-05-07 20:09:23 +0700

    Initial
    Initial

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('#tombolWA').floatingWhatsApp({
+        phone: '6285343900900',
+        popupMessage: 'Selamat datang di Agaris Agrobiz, dapatkan bantuan dengan membalas pesan ini, kami siap melayani Anda. Terima kasih.',
+        message: "Apakah ada stok jamur kancing?",
+        showPopup: true,
+        showOnIE: false,
+        headerTitle: 'Agaris Agrobiz',
+        headerColor: '#b6c7c1',
+        // backgroundColor: 'crimson',
+        buttonImage: '<img src="img/whatsapp.svg" />'
+    });
+});
